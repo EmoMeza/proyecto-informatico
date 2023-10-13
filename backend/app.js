96ca405b28af412fa14c53eb990491fa1b3605d8
EmoMeza <emeza2019@inf.udec.cl>
2023-10-13 01:03:58 -0300

    ✨ (backend) enpoints added + updated postman json

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { json } = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ServerApiVersion = require('mongodb'). ServerApiVersion;
+const { ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express(); // Inicializamos express.
@@ -33,42 +34,18 @@ app.use(express.json()) // Nos permite trabajar con el formato json.
 
 
 // Rutas
-app.post('/', async function (req, res) {
-    const nombre = req.query.name;
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        const database = client.db("Proyecto_Informatico"); //schema
-        const collection = database.collection("Enevs"); //table
-        // Check if the nombre already exists in the collection
-        const result = await collection.findOne({ nombre: nombre });
-        if (result) {
-            // If the nombre already exists, send a message to the client
-            res.send(`El nombre ${nombre} ya existe en la base de datos`);
-        } else {
-            // If the nombre doesn't exist, insert it into the collection
-            const insertResult = await collection.insertOne({ nombre: nombre });
-            // Send the result to the client
-            res.send(insertResult);
-        }
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-});
-
 app.get('/get/evento', async function (req, res) {
-    const nombre = req.query.nombre;
+    const id = req.query.id;
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        const database = client.db("Proyecto_Informatico"); //schema
+        const database = client.db("proyecto_informatico"); //schema
         const collection = database.collection("test"); //table
-        // Check if the nombre already exists in the collection
-        const result = await collection.findOne({ nombre: nombre });
+        // Check if the id already exists in the collection
+        const result = await collection.findOne({ _id: new ObjectId(id) });
         if (!result) {
-            // If the nombre already exists, send a message to the client
-            res.send(`El nombre ${nombre} no existe en la base de datos`);
+            // If the id already exists, send a message to the client
+            res.send(`El id ${id} no existe en la base de datos`);
         }
         else{
             //return the whole row of the collection
@@ -85,62 +62,55 @@ app.post('/add/evento', async function (req, res) {
     const data = req.body;
     try{
         await client.connect();
-        const database = client.db("Proyecto_Informatico");
+        const database = client.db("proyecto_informatico");
         const collection = database.collection("test");
-        //check if the nombre already exists in the collection, if exists send a message to the client and exit
-        const result = await collection.findOne({ nombre: data.nombre });
-        if(result){
-            res.send(`El nombre ${data.nombre} ya existe en la base de datos`);
-        }
-        else{
-            //if the nombre doesn't exist, insert it into the collection
-            await collection.insertOne(data);
-            //send the result to the client
-            res.send("se ha insertado correctamente");
-        }
+        //if the nombre doesn't exist, insert it into the collection
+        await collection.insertOne(data);
+        //send the result to the client
+        res.send("se ha insertado correctamente");
     } finally {
         await client.close();
     }
 });
 
 app.put('/update/evento', async function (req, res) {
-    const nombre = req.query.nombre;
+    const id = req.query.id;
     const data = req.body;
     try{
         await client.connect();
-        const database = client.db("Proyecto_Informatico");
+        const database = client.db("proyecto_informatico");
         const collection = database.collection("test");
-        //check if the nombre already exists in the collection, if exists send a message to the client and exit
-        const result = await collection.findOne({ nombre: nombre });
+        //check if the id already exists in the collection, if exists send a message to the client and exit
+        const result = await collection.findOne({ _id: new ObjectId(id) });
         const result2 = await collection.findOne({ nombre: data.nombre });
         if(!result || result2){
-            res.send(`El nombre ${nombre} no existe en la base de datos o el nuevo nombre ya existe`);
+            res.send(`El id ${id} no existe en la base de datos o el nuevo nombre ya existe`);
         }
         else{
-            //if the nombre doesn't exist, insert it into the collection
-            await collection.updateOne({nombre: nombre}, {$set: data});
+            //if the id doesn't exist, insert it into the collection
+            await collection.updateOne({_id: new ObjectId(id)}, {$set: data});
             //send the result to the client
             res.send("se ha actualizado correctamente");
         }
     } finally {
         await client.close();
     }
-});
+}); 
 
 app.delete('/delete/evento', async function (req, res) {
-    const nombre = req.query.nombre;
+    const id = req.query.id;
     try{
         await client.connect();
-        const database = client.db("Proyecto_Informatico");
+        const database = client.db("proyecto_informatico");
         const collection = database.collection("test");
-        //check if the nombre already exists in the collection, if exists send a message to the client and exit
-        const result = await collection.findOne({ nombre: nombre });
+        //check if the id already exists in the collection, if exists send a message to the client and exit
+        const result = await collection.findOne({ _id: new ObjectId(id) });
         if(!result){
-            res.send(`El nombre ${nombre} no existe en la base de datos`);
+            res.send(`El id ${id} no existe en la base de datos`);
         }
         else{
-            //if the nombre doesn't exist, insert it into the collection
-            await collection.deleteOne({nombre: nombre});
+            //if the id doesn't exist, insert it into the collection
+            await collection.deleteOne({_id: new ObjectId(id)});
             //send the result to the client
             res.send("se ha eliminado correctamente");
         }
@@ -152,7 +122,7 @@ app.delete('/delete/evento', async function (req, res) {
 app.get('/get/all/eventos', async function (req, res) {
     try{
         await client.connect();
-        const database = client.db("Proyecto_Informatico");
+        const database = client.db("proyecto_informatico");
         const collection = database.collection("test");
         //check if the nombre already exists in the collection, if exists send a message to the client and exit
         const result = await collection.find().toArray();
@@ -166,7 +136,7 @@ app.get('/get/all/eventos', async function (req, res) {
     } finally {
         await client.close();
     }
-});   
+});
 
 app.get('/get/filter/eventos', async function (req, res) {
     const categoria = req.query.categoria;
@@ -175,7 +145,7 @@ app.get('/get/filter/eventos', async function (req, res) {
     }else{
         try{
             await client.connect();
-            const database = client.db("Proyecto_Informatico");
+            const database = client.db("proyecto_informatico");
             const collection = database.collection("test");
             //check if the nombre already exists in the collection, if exists send a message to the client and exit
             const result = await collection.find({categoria: categoria}).toArray();
@@ -195,8 +165,8 @@ app.get('/get/alumno', async function (req, res) {
     const matricula = req.query.matricula;
     try {
       await client.connect();
-      const database = client.db("Proyecto_Informatico");
-      const collection = database.collection("Alumnos");
+      const database = client.db("proyecto_informatico");
+      const collection = database.collection("alumnos");
       // Check if the alumno already exists in the collection
       const result = await collection.findOne({ matricula: matricula });
       if (!result) {
@@ -221,8 +191,8 @@ app.post('/add/alumno', async function (req, res) {
     data.matricula = matricula;
     try {
       await client.connect();
-      const database = client.db("Proyecto_Informatico");
-      const collection = database.collection("Alumnos");
+      const database = client.db("proyecto_informatico");
+      const collection = database.collection("alumnos");
   
       // Check if the alumno already exists in the collection
       const result = await collection.findOne({ nombre: data.nombre });
@@ -247,8 +217,8 @@ app.put('/update/alumno', async function (req, res) {
 
     try{
         await client.connect();
-        const database = client.db("Proyecto_Informatico");
-        const collection = database.collection("Alumnos");
+        const database = client.db("proyecto_informatico");
+        const collection = database.collection("alumnos");
         //check if the nombre already exists in the collection, if exists send a message to the client and exit
         const result = await collection.findOne({ matricula: matricula });
         if(!result){
@@ -265,9 +235,223 @@ app.put('/update/alumno', async function (req, res) {
     }
 });
 
+app.delete('/delete/alumno', async function (req, res) {
+    const matricula = req.query.matricula;
+    try{
+        await client.connect();
+        const database = client.db("proyecto_informatico");
+        const collection = database.collection("alumnos");
+        //check if the nombre already exists in the collection, if exists send a message to the client and exit
+        const result = await collection.findOne({ matricula: matricula });
+        if(!result){
+            res.send(`El alumno con matricula ${matricula} no existe en la base de datos`);
+        }
+        else{
+            //if the nombre doesn't exist, insert it into the collection
+            await collection.deleteOne({matricula: matricula});
+            //send the result to the client
+            res.send("se ha eliminado correctamente");
+        }
+    } finally {
+        await client.close();
+    }
+});
+
+// Hay que arreglar los ingresos y egresos ya que el length no se puede medir si no hay nada en el array y tira error
+// falta que despues de cada movimiento se calcule el total y se actualice en la base de datos
+
+app.get('/get/all/ingresos', async function (req, res) {
+    const id = req.query.id; // assuming the query parameter is named "id"
+    try {
+        await client.connect();
+        const database = client.db("proyecto_informatico");
+        const collection = database.collection("test");
+        // Check if the document already exists in the collection
+        const result = await collection.findOne({ _id: new ObjectId(id) }); // using ObjectId to convert the string id to a BSON ObjectId
+        if (!result) {
+            // If the document doesn't exist, send a message to the client
+            res.send(`El evento con el id "${id}" no existe en la base de datos`);
+        } else {
+            // sum the first position of the arrays of ingresos and returns the result
+            var suma = 0;
+            for (var i = 0; i < result.ingresos.length; i++) {
+                    if (result.ingresos[i] != null){
+                            suma += result.ingresos[i][0];
+                    }
+            }
+            res.send(`El total de ingresos del evento ${result.nombre} es: ${suma}`); // using the "nombre" field from the result document
+        }
+    } finally {
+        await client.close();
+    }
+});
+
+app.post('/add/ingreso', async function (req, res) {
+    const id = req.query.id;
+    const data = req.body;
+    const data2 = data.ingresos;
+    console.log(data2);
+    try{
+        await client.connect();
+        const database = client.db("proyecto_informatico");
+        const collection = database.collection("test");
+        //check if the id already exists in the collection, if exists send a message to the client and exit
+        const result = await collection.findOne({ _id: new ObjectId(id) });
+        if(result){
+            // if the id exists, add the data to the array named ingresos inside the one that has the same id
+            await collection.updateOne({_id: new ObjectId(id)}, {$push: {ingresos: data2}});
+            //send the result to the client
+            res.send("se ha insertado correctamente");   
+        }
+        else{
+            res.send(`El id ${id} no existe en la base de datos`);
+        }
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/get/all/egresos', async function (req, res) {
+        const id = req.query.id;
+        try {
+            await client.connect();
+            const database = client.db("proyecto_informatico");
+            const collection = database.collection("test");
+            // Check if the document already exists in the collection
+            const result = await collection.findOne({ _id: new ObjectId(id) });
+            if (!result) {
+                // If the document doesn't exist, send a message to the client
+                res.send(`El evento con el id "${id}" no existe en la base de datos`);
+            } else {
+                // sum the first position of the arrays of egresos and returns the result
+                var suma = 0;
+                for (var i = 0; i < result.egresos.length; i++) {
+                        if (result.egresos[i] != null){
+                                suma += result.egresos[i][0];
+                        }
+                }
+                res.send(`El total de egresos del evento ${result.nombre} es: ${suma}`);
+            }
+        } finally {
+            await client.close();
+        }
+});
+
+app.post('/add/egreso', async function (req, res) {
+        const id = req.query.id;
+        const data = req.body;
+        const data2 = data.egresos;
+        console.log(data2);
+        try{
+                await client.connect();
+                const database = client.db("proyecto_informatico");
+                const collection = database.collection("test");
+                //check if the id already exists in the collection, if exists send a message to the client and exit
+                const result = await collection.findOne({ _id: new ObjectId(id) });
+                if(result){
+                        // if the id exists, add the data to the array named egresos inside the one that has the same id
+                        await collection.updateOne({_id: new ObjectId(id)}, {$push: {egresos: data2}});
+                        //send the result to the client
+                        res.send("se ha insertado correctamente");   
+                }
+                else{
+                        res.send(`El id ${id} no existe en la base de datos`);
+                }
+        } finally {
+                await client.close();
+        }
+});
+
+app.get('/get/total', async function (req, res) {
+        //total is all ingresos - all egresos
+        const id = req.query.id;
+        try {
+            await client.connect();
+            const database = client.db("proyecto_informatico");
+            const collection = database.collection("test");
+            // Check if the document already exists in the collection
+            const result = await collection.findOne({ _id: new ObjectId(id) });
+            if (!result) {
+                // If the document doesn't exist, send a message to the client
+                res.send(`El evento con el id "${id}" no existe en la base de datos`);
+            } else {
+                // sum the first position of the arrays of ingresos and returns the result
+                var suma = 0;
+                for (var i = 0; i < result.ingresos.length; i++) {
+                        if (result.ingresos[i] != null){
+                                suma += result.ingresos[i][0];
+                        }
+                }
+                var suma2 = 0;
+                for (var i = 0; i < result.egresos.length; i++) {
+                        if (result.egresos[i] != null){
+                                suma2 += result.egresos[i][0];
+                        }
+                }
+                res.send(`El total de ingresos del evento ${result.nombre} es: ${suma-suma2}`);
+            }
+        } finally {
+            await client.close();
+        }
+});
+
+
+app.get('/get/all/asistencias', async function (req, res) {
+    const id = req.query.id;
+    try{
+        await client.connect();
+        const database = client.db("proyecto_informatico");
+        const collection = database.collection("test");
+        const result = await collection.findOne({ _id: new ObjectId(id) });
+        if(!result){
+            res.send(`El id ${id} no existe en la base de datos`);
+        }
+        else{
+            res.send(result.asistencia);
+        }
+    } finally {
+        await client.close();
+    }
+});
+
+app.post('/add/asistencia', async function (req, res) {
+    const id = req.query.id;
+    const matricula = req.query.matricula;
+
+    try{
+        await client.connect();
+        const database = client.db("proyecto_informatico");
+        const collection = database.collection("test");
+        const result = await collection.findOne({ _id: new ObjectId(id) });
+        const collection2 = database.collection("alumnos");
+        const result2 = await collection2.findOne({ matricula: matricula });
+        //check if the matricula already exists in the collection, if exists send a message to the client and exit
+        if(!result2){
+            res.send(`El alumno con matricula ${matricula} no existe en la base de datos`);             
+        }
+        else{
+            if(!result){
+                res.send(`El id ${id} no existe en la base de datos`);
+            }
+            else{
+                //if the matricula doesn't exist in the collection, insert it into the collection
+                if(result.asistencia.includes(matricula)){
+                    res.send(`El alumno con matricula ${matricula} ya esta registrado en el evento`);
+                }else{
+                    await collection.updateOne({_id: new ObjectId(id)}, {$push: {asistencia: matricula}});
+                    res.send("se ha insertado correctamente");
+                }
+            }
+        }
+    } finally {
+        await client.close();
+    }
+});
+
+
+
 const puerto = process.env.PORT || 4040;
 
 app.listen(puerto, function () {
-    // console.log('Example app listening on port '+ puerto);
-    console.log(`Example app listening on port ${puerto}!`);
+    console.log(`Servidor escuchando en el puerto: ${puerto}!`);
 });

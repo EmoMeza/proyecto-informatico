49af1014198fa476c70675dad1b7be78b37259f0
Carlos Venegas <cvenegasa@udec.cl>
2023-10-16 23:52:56 -0300

    ✨ (frontend): Agregar rutas de paginas al main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_informatico/pages/login.dart';
+import 'package:proyecto_informatico/pages/menuCAA.dart';
+import 'package:proyecto_informatico/pages/menuAlumnos.dart';
+import 'package:proyecto_informatico/pages/agregarEvento.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +17,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'EventiCAA',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple, // Set the primary swatch color
+        ).copyWith(secondary: Colors.deepPurple), // Set the secondary color
       ),
-      home: const Login(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const Login(),
+        '/menuCAA': (context) => menuCAA(),
+        '/menuAlumnos': (context) => menuAlumnos(),
+        '/agregarEvento': (context) => AgregarEvento(),
+      },
     );
   }
 }
@@ -44,15 +54,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(

5280df12513c0cdfba69e1c2f72d37d1f16f580a
dimaldonado <102496218+dimaldonado@users.noreply.github.com>
2023-10-18 21:25:55 -0300

    :sparkles: (frontend) : added list of events when selecting a day in the calendar

--- frontend/lib/pages/calendario.dart
+++ frontend/lib/pages/calendario.dart
@@ -27,66 +27,109 @@ class Calendario extends StatefulWidget {
   _CalendarioState createState() => _CalendarioState();
 }
 
+
 class _CalendarioState extends State<Calendario> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  Random random =  Random();
-  //para testear
+  Random random = Random();
+
   List<Evento> eventos = [
     Evento(
-      nombre: 'Final de Proyecto',
-      categoria: 'Academico',
-      descripcion: 'Examen final del curso de matemáticas.',
+      nombre: 'Completada bailable',
+      categoria: 'Actividad',
+      descripcion: 'Venta de completos CAINF',
       fechaInicio: DateTime(2023, 10, 20),
       fechaFinal: DateTime(2023, 10, 22),
-      visible: true,
+      visible: false,
     ),
     Evento(
-      nombre: 'Final de Proyecto',
-      categoria: 'Academico',
-      descripcion: 'Examen final del curso de matemáticas.',
+      nombre: 'Tarreo Infomatica',
+      categoria: 'Actividad',
+      descripcion: 'Actividad de recreacion para los alumnos de informatica',
       fechaInicio: DateTime(2023, 10, 10),
       fechaFinal: DateTime(2023, 10, 11),
       visible: true,
     ),
-  // Agrega más eventos según sea necesario
+    Evento(
+      nombre: 'Carrete mechon',
+      categoria: 'Actividad',
+      descripcion: 'Actividad de recreacion para nuevos alumnos de informatica',
+      fechaInicio: DateTime(2023, 10, 20),
+      fechaFinal: DateTime(2023, 10, 21),
+      visible: true,
+    ),
+    
+    // Agrega más eventos según sea necesario
   ];
 
+  List<Evento> _getEventosDelDia(DateTime day) {
+  // Filtra los eventos que caen en el rango de fechas de inicio y fin, incluyendo ambos extremos
+  return eventos.where((evento) {
+    return day.isAtSameMomentAs(evento.fechaInicio) ||
+        day.isAtSameMomentAs(evento.fechaFinal) ||
+        (day.isAfter(evento.fechaInicio) && day.isBefore(evento.fechaFinal));
+  }).toList();
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Caledario de eventos'),
+        title: const Text('Calendario de eventos'),
       ),
-      body: TableCalendar(
-        headerStyle: const HeaderStyle(titleCentered: true,formatButtonVisible: false),
-        locale: 'es_ES',
-        firstDay: DateTime.utc(2023, 1, 1),
-        lastDay: DateTime.utc(2030, 12, 31),
-        focusedDay: _focusedDay,
-        calendarFormat: _calendarFormat,
-        selectedDayPredicate: (day) {
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          setState(() {
-            _selectedDay = selectedDay;
-            _focusedDay = focusedDay; // update `_focusedDay` here as well
-          });
-        },
-        eventLoader: (day) {
-          // Filtra los eventos que caen en el rango de fecha de inicio y fecha de fin, incluyendo el día de inicio y fin
-          var events = eventos.where((evento) {
-            return (evento.fechaInicio.isBefore(day) || evento.fechaInicio.isAtSameMomentAs(day)) &&
-                (evento.fechaFinal.isAfter(day) || evento.fechaFinal.isAtSameMomentAs(day));
-          }).toList();
+      body: Column(
+        children: [
+          TableCalendar(
+            headerStyle: const HeaderStyle(titleCentered: true, formatButtonVisible: false),
+            locale: 'es_ES',
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            eventLoader: (day) {
+              var events = eventos.where((evento) {
+                return (evento.fechaInicio.isBefore(day) || evento.fechaInicio.isAtSameMomentAs(day)) &&
+                    (evento.fechaFinal.isAfter(day) || evento.fechaFinal.isAtSameMomentAs(day));
+              }).toList();
+              return events;
+            },
+            calendarStyle: CalendarStyle(
+              weekendTextStyle: const TextStyle().copyWith(color: Colors.red),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _selectedDay != null ? _buildEventList(_getEventosDelDia(_selectedDay!)) : Container(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-          return events;
-        },
-       
-      ),
+  Widget _buildEventList(List<Evento> eventosDelDia) {
+    return ListView.builder(
+      itemCount: eventosDelDia.length,
+      itemBuilder: (context, index) {
+        Evento evento = eventosDelDia[index];
+        return ListTile(
+          title: Text(evento.nombre),
+          subtitle: Text(evento.descripcion),
+          // Puedes mostrar más detalles del evento si es necesario
+        );
+      },
     );
   }
 }

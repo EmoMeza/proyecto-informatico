8c081adb46ef5c45f6e377e00822196c9c765043
dimaldonado <102496218+dimaldonado@users.noreply.github.com>
2023-11-18 04:46:36 -0300

    ✨(Frontend) : Added functionality to export attendance lists in pdf and excel format.

--- frontend/lib/pages/lista_asistencia.dart
+++ frontend/lib/pages/lista_asistencia.dart
@@ -1,98 +1,12 @@
-import 'package:flutter/material.dart';
 import '../api_services.dart';
-
-class ListaAsistenciaPage extends StatefulWidget {
-  final String eventId;
-
-  ListaAsistenciaPage(this.eventId);
-
-  @override
-  _ListaAsistenciaPageState createState() => _ListaAsistenciaPageState();
-}
-
-class _ListaAsistenciaPageState extends State<ListaAsistenciaPage> {
-  List<String> asistencias = [];
-  List<AlumnoInfo> alumnosInfo = [];
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadAsistencias();
-  }
-
-  Future<void> _loadAsistencias() async {
-    ApiResponse response = await ApiService.getAsistenciasEvento(widget.eventId);
-
-    setState(() {
-      if (response.success && response.data is List) {
-        asistencias = (response.data as List<dynamic>).map((e) => e.toString()).toList();
-        _loadAlumnosInfo();
-      } else {
-        asistencias = [];
-        isLoading = false;
-      }
-    });
-  }
-
-  Future<void> _loadAlumnosInfo() async {
-    List<AlumnoInfo> alumnos = [];
-
-    for (String matricula in asistencias) {
-      ApiResponse response = await ApiService.getAlumno(matricula);
-
-      if (response.success && response.data != null) {
-        alumnos.add(AlumnoInfo.fromJson(response.data));
-      }
-    }
-
-    setState(() {
-      alumnosInfo = alumnos;
-      isLoading = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista de Asistencia'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (isLoading)
-              Column(
-                children: [
-                  CircularProgressIndicator(),
-                  SizedBox(height: 8.0),
-                  Text('Cargando asistencias...'),
-                ],
-              ),
-            if (!isLoading)
-              Expanded(
-                child: DataTable(
-                  columns: [
-                    DataColumn(label: Text('Matrícula')),
-                    DataColumn(label: Text('Nombre')),
-                    DataColumn(label: Text('Apellido')),
-                  ],
-                  rows: alumnosInfo.map((alumno) {
-                    return DataRow(cells: [
-                      DataCell(Text(alumno.matricula)),
-                      DataCell(Text(alumno.nombre)),
-                      DataCell(Text(alumno.apellido)),
-                    ]);
-                  }).toList(),
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'dart:typed_data';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
+import 'package:excel/excel.dart';
 
 class AlumnoInfo {
   final String matricula;
@@ -113,3 +27,182 @@ class AlumnoInfo {
     );
   }
 }
+
+
+class ListaAsistenciaPage extends StatefulWidget {
+  final String eventId;
+
+  ListaAsistenciaPage(this.eventId);
+
+  @override
+  _ListaAsistenciaPageState createState() => _ListaAsistenciaPageState();
+}
+
+class _ListaAsistenciaPageState extends State<ListaAsistenciaPage> {
+  List<String> asistencias = [];
+  List<AlumnoInfo> alumnosInfo = [];
+  bool isLoading = true;
+  String nombreEvento = '';
+  late String eventId;
+  @override
+  void initState() {
+    super.initState();
+    eventId = widget.eventId;
+    _getEventData();
+
+  }
+  Future<void> _getEventData() async{
+    ApiResponse response = await ApiService.getEvento(eventId);
+    if (response.success) {
+      // Muestra una ventana emergente con un mensaje
+      setState(() {
+        nombreEvento = response.data['nombre'];
+        asistencias = (response.data['asistencia'] as List<dynamic>).map((e) => e.toString()).toList();
+        _loadAlumnosInfo();
+      });
+    }
+  }
+  
+
+  Future<void> _loadAlumnosInfo() async {
+    List<AlumnoInfo> alumnos = [];
+
+    for (String matricula in asistencias) {
+      debugPrint(matricula);
+      ApiResponse response = await ApiService.getAlumno(matricula);
+
+      if (response.success && response.data != null) {
+        alumnos.add(AlumnoInfo.fromJson(response.data));
+      }
+    }
+    setState(() {
+      alumnosInfo = alumnos;
+      isLoading = false;
+    });
+  }
+  
+  Future<void> _exportToExcel() async {
+    var excel = Excel.createExcel();
+    var sheet = excel['ListaAsistencia'];
+
+    // Headers
+    sheet.appendRow(['Matrícula', 'Nombre', 'Apellido']);
+
+    // Data
+    for (var alumno in alumnosInfo) {
+      sheet.appendRow([alumno.matricula, alumno.nombre, alumno.apellido]);
+    }
+
+   // Guarda el archivo Excel en la memoria del dispositivo
+    final directory = await getExternalStorageDirectory();
+    final filePath = '${directory!.path}/lista_asistencia-$nombreEvento.xlsx';
+
+    // Usa 'encode' en lugar de 'save'
+    final fileBytes = excel.encode();
+
+    if (fileBytes != null) {
+      await File(filePath).writeAsBytes(fileBytes);
+
+      // Comparte el archivo Excel
+      Share.shareFiles([filePath], text: 'Lista de Asistencia - $nombreEvento');
+    } else {
+      // Handle the case where fileBytes is null (optional)
+      print('Error creating Excel file');
+    }
+  }
+
+  
+  Future<void> _exportToPdf() async {
+    final pdf = pw.Document();
+
+    // Agrega una página al PDF
+    pdf.addPage(
+      pw.MultiPage(
+        build: (context) => [
+          pw.Header(level: 1, text: 'Lista de Asistencia $nombreEvento'),
+          pw.SizedBox(height: 10),
+          pw.TableHelper.fromTextArray(
+            context: context,
+            cellAlignment: pw.Alignment.center,
+            cellStyle: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+            headerDecoration: const pw.BoxDecoration(
+              color: PdfColors.grey300,
+            ),
+            data: <List<String>>[
+              ['Matrícula', 'Nombre', 'Apellido'],
+              for (var alumno in alumnosInfo)
+                [alumno.matricula, alumno.nombre, alumno.apellido],
+            ],
+          ),
+        ],
+      ),
+    );
+
+    // Obtiene el directorio temporal en el dispositivo móvil
+    final directory = await getTemporaryDirectory();
+
+    // Crea la ruta del archivo
+    final filePath = '${directory.path}/lista_asistencia.pdf';
+
+    // Guarda el PDF en la ruta del archivo
+    final file = File(filePath);
+    await file.writeAsBytes(await pdf.save());
+
+    // Comparte el archivo PDF
+    Share.shareFiles([filePath], text: 'Lista de Asistencia - $nombreEvento');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Asistencia'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.picture_as_pdf),
+            onPressed:() {
+              _exportToPdf();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.table_chart),
+            onPressed: _exportToExcel,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (isLoading)
+              const Column(
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 8.0),
+                  Text('Cargando asistencias...'),
+                ],
+              ),
+            if (!isLoading)
+              Expanded(
+                child: DataTable(
+                  columns: const [
+                    DataColumn(label: Text('Matrícula')),
+                    DataColumn(label: Text('Nombre')),
+                    DataColumn(label: Text('Apellido')),
+                  ],
+                  rows: alumnosInfo.map((alumno) {
+                    return DataRow(cells: [
+                      DataCell(Text(alumno.matricula)),
+                      DataCell(Text(alumno.nombre)),
+                      DataCell(Text(alumno.apellido)),
+                    ]);
+                  }).toList(),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+

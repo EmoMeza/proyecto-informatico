e7fdca479e2523a97274856075afd323a372d3a8
miosv <mvilla2019@inf.udec.cl>
2023-11-22 00:14:26 -0300

    ✨ (frontend) post y get evento agrega imagen

--- frontend/lib/pages/agregarEvento.dart
+++ frontend/lib/pages/agregarEvento.dart
@@ -195,7 +195,7 @@ class _AgregarEventoState extends State<AgregarEvento> {
       };
 
       ApiResponse response =
-          await ApiService.postEvento("652976834af6fedf26f3493d", postData);
+          await ApiService.postEvento("652976834af6fedf26f3493d", postData, null);
       if (response.success) {
         showResponseDialog(context, response.message, response.success);
       } else {
